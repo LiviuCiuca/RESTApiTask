c26f60236eed5ec2162a1e3269d35ccaf39ee2d9
Liviu Ciuca <lc874@canterbury.ac.uk>
2022-10-03 18:54:34 +0000

    followed tutorials for express.js

--- app/server.js
+++ app/server.js
@@ -2,64 +2,32 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-// Init Express ----------------------------------------------------------------
-
-app.use(express.json());  // Enable json input from incoming requests. This is accessible from `req.body`
-
-// CORS - https://expressjs.com/en/resources/middleware/cors.html
-const cors = require('cors')
-app.use(cors())
-
-// Routes ----------------------------------------------------------------------
+app.use(express.json());
+attendies = []
 
 app.get('/', (req, res) => {
-  res.sendFile('client.html', {root: __dirname})
+  res.send('<html><body>Your HTML text</body></html>')
 })
-//app.get('/vue.js', (req, res) => {
-//  res.sendFile('vue.js', {root: __dirname})
-//})
 
-ATTENDEES = []
-
-app.post('/attendee/', (req, res) => {
-  if (Object.keys(req.body).sort().toString() != 'id,name,notes') {
-    return res.status(405).json({message: 'missing fields'})
-  }
-  ATTENDEES.push(req.body)
+app.post('/attendee', (req,res)=>{
+  attendies.push(req.body)
   res.status(201).json(req.body)
+  res.json(req.body)
 })
 
-app.get('/attendees/', (req, res) => {
-  res.status(200).json(ATTENDEES)
+app.get('/attendees', (req, res) => {
+  res.json(attendies)
 })
 
 app.delete('/attendee/:id', (req, res) => {
-  const id = parseFloat(req.params.id)
-  ATTENDEES = [...ATTENDEES.filter((attendee)=>attendee.id != id)]
-  res.status(204).json({})
-  // TODO: implement 404?
+  attendies = attendies.filter(o => o.id !== parseFloat(req.params.id))
+  res.status(204).json()
+  
 })
-
-
-// Serve -----------------------------------------------------------------------
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
 
-/*
-// https://expressjs.com/en/guide/error-handling.html
-function logErrors (err, req, res, next) {
-  console.error(err.stack)
-  next(err)
-}
-app.use(logErrors)
-app.use(function(req, res, next){
-  console.log('no route', req.originalUrl);
-  res.status(404).type('txt').send('Not found');
-  next()
-})
-*/
 
 // Docker container exit handler - https://github.com/nodejs/node/issues/4182
 process.on('SIGINT', function() {process.exit()})
